9153e95ad46ae3bcd1bcb7b536bc48aeccae5633
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-12-27 16:31:39 +0530

    SoftDelete added

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -26,7 +26,9 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         return user
 
--- upload_csv_file/views.py
+++ upload_csv_file/views.py
@@ -7,15 +7,12 @@ from rest_framework import status, generics
 from .models import Order
 from accounts.models import CustomUser
 from .serializers import OrderSerializer
-from rest_framework import filters as rest_filters
 from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer, UserActivationSerializer
 from accounts.serializers import CustomUserSerializer
 from .filters import OrderFilter
-from rest_framework.pagination import PageNumberPagination
-from django_filters import rest_framework as filters
-from rest_framework.filters import OrderingFilter 
+from rest_framework.pagination import PageNumberPagination 
 from rest_framework.permissions import IsAdminUser
 
 
@@ -38,12 +35,11 @@ class OrderUpdateView(generics.UpdateAPIView):
 
 class OrderDeleteView(APIView):
     def delete(self, request, pk):
-        try:
-            order = Order.objects.get(pk=pk)
-            order.is_delete = True
-            order.save()
+        order = Order.objects.filter(pk=pk).first()
+        if order:
+            order.delete()
             return Response({"message": "Order deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-        except Order.DoesNotExist:
+        else:
             return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class OrderPagination(PageNumberPagination):
@@ -54,7 +50,6 @@ class OrderPagination(PageNumberPagination):
 class OrderListView(generics.ListAPIView):
     queryset = Order.objects.filter(is_delete=False)
     serializer_class = OrderSerializer
-    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
     filterset_class = OrderFilter
     ordering_fields = ['order_date', 'ship_date', 'order_priority', 'sales_channel']
     pagination_class = OrderPagination
@@ -67,25 +62,26 @@ class AdminOrderListView(generics.ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
+    permission_classes = [IsAdminUser]
+    filterset_class = OrderFilter
+    ordering_fields = ['order_date', 'ship_date', 'order_priority', 'sales_channel']
 
-    def get(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return Response({"error": "Permission denied"}, status=status.HTTP_403_FORBIDDEN)
-        response = self.list(request, *args, **kwargs)
+    def list(self, request, *args, **kwargs): 
+        response = super().list(request, *args, **kwargs)
         return Response({"message": "Admin order list retrieved successfully", "data": response.data}, status=response.status_code)
 
 class UserActivationView(APIView):
     permission_classes = [IsAdminUser]
 
     def patch(self, request, pk):
-        try:
-            user = CustomUser.objects.get(pk=pk)
+        user = CustomUser.objects.filter(pk=pk).first()
+        if user:
             serializer = UserActivationSerializer(user, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
                 return Response({"message": "User activation status updated successfully", "data": serializer.data})
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except CustomUser.DoesNotExist:
+        else: 
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class UserListView(generics.ListAPIView):
